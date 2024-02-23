30275fb6e229acc18b9ef42c90eabedcd44f217f
Megan <choomegan@gmail.com>
2024-02-23 17:36:48 +0800

    refactor: load model from filepath not hf

--- src/vad.py
+++ src/vad.py
@@ -1,31 +1,34 @@
 """
 VAD for audio clips
 """
+
 import os
 import csv
+import logging
 import tqdm
 import torch
-from dotenv import load_dotenv
+import hydra
+from omegaconf import DictConfig
 from pyannote.audio import Model
 from pyannote.audio.pipelines import VoiceActivityDetection
 
+logging.basicConfig(
+    format="%(levelname)s | %(asctime)s | %(message)s", level=logging.INFO
+)
 
-def get_vad_segments(
-    model: str,
-    root_dir: str,
-):
-    """
-    Expected folder structure of root_dir:
-    root_dir / subdir  / audio file 1
-             / subdir2 / audio file 2
-    """
 
-    model = Model.from_pretrained(model, use_auth_token=HF_AUTH_TOKEN)
+@hydra.main(config_path="../conf", config_name="vad")
+def get_vad_segments(cfg: DictConfig):
+
+    logging.info("loading model...")
+
+    model = Model.from_pretrained(cfg.vad.model)
     model.to(torch.device("cuda"))
 
-    pipeline = VoiceActivityDetection(segmentation=model)
+    pipeline = VoiceActivityDetection(segmentation=cfg.vad.model)
     pipeline.to(torch.device("cuda"))
 
+    logging.info("loaded vad model!")
     HYPER_PARAMETERS = {
         # remove speech regions shorter than that many seconds.
         "min_duration_on": 0.0,
@@ -34,20 +37,19 @@ def get_vad_segments(
     }
     pipeline.instantiate(HYPER_PARAMETERS)
 
-    subdirs = os.listdir(root_dir)
+    audio_files = os.listdir(cfg.vad.audio_dir)
 
-    os.makedirs("./output", exist_ok=True)
-    for folder_name in tqdm.tqdm(subdirs):
-        curr_dir = os.path.join(root_dir, folder_name)
-        vid_name = os.listdir(curr_dir)[0]
-        audio_dir = os.path.join(curr_dir, vid_name)
+    os.makedirs("/workspace/output", exist_ok=True)
+    for filename in tqdm.tqdm(audio_files):
+        curr_dir = os.path.join(cfg.vad.audio_dir, filename)
+        vid_name = os.path.splitext(filename)[0]
 
-        with open(f"./output/{os.path.splitext(vid_name)[0]}.txt", "w") as out_file:
+        with open(f"/workspace/output/{vid_name}.txt", "w") as out_file:
             writer = csv.writer(
                 out_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
             )
             vad = pipeline(
-                audio_dir
+                curr_dir
             )  # returns pyannote Annotation object (https://github.com/pyannote/pyannote-core/blob/develop/pyannote/core/annotation.py)
 
             # v_tracks returns sorted dictionary
@@ -59,11 +61,4 @@ def get_vad_segments(
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    AUDIO_DIR = os.getenv("AUDIO_DIR")
-
-    get_vad_segments(
-        model="pyannote/segmentation-3.0",
-        root_dir=AUDIO_DIR,
-    )
+    get_vad_segments()
