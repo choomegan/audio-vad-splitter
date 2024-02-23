2b7748482d24609b567fc7151dfc294d1c7d270c
Megan <choomegan@gmail.com>
2024-02-23 17:35:56 +0800

    refactor: change from env variable to hydra

--- src/split_by_vad.py
+++ src/split_by_vad.py
@@ -1,21 +1,25 @@
 """
 Split audio file based on VAD segments
 """
+
 import os
 import tqdm
-from dotenv import load_dotenv
+
+import hydra
+from omegaconf import DictConfig
 from pydub import AudioSegment
 
 
-def split_audio(vad_dir: str, audio_dir: str, outdir_dir: str):
-    os.makedirs(outdir_dir, exist_ok=True)
+@hydra.main(config_path="../conf", config_name="vad")
+def split_audio(cfg: DictConfig):
+    os.makedirs(cfg.split.out_dir, exist_ok=True)
 
-    for txt_file in tqdm.tqdm(os.listdir(vad_dir)):
+    for txt_file in tqdm.tqdm(os.listdir(cfg.split.vad_segments)):
         audio_name = os.path.splitext(txt_file)[0]
-        audio_filepath = os.path.join(audio_dir, f"{audio_name}.wav")
+        audio_filepath = os.path.join(cfg.vad.audio_dir, f"{audio_name}.wav")
 
         audio = AudioSegment.from_wav(audio_filepath)
-        with open(os.path.join(vad_dir, txt_file), "r") as file:
+        with open(os.path.join(cfg.split.vad_segments, txt_file), "r") as file:
             for idx, line in enumerate(file):
                 split_line = line.split(",")
                 t1 = float(split_line[0]) * 1000  # milliseconds
@@ -24,23 +28,16 @@ def split_audio(vad_dir: str, audio_dir: str, outdir_dir: str):
                 new_filename = f"{audio_name}-{idx}.wav"
 
                 # export audio file to output dir
-                audio_segment.export(f"{outdir_dir}/{new_filename}")
+                audio_segment.export(f"{cfg.split.out_dir}/{new_filename}")
 
-            try:
-                os.remove(audio_filepath)
-                print(f"Removed original video")
-            except Exception as e:
-                print(f"Failed to remove original video, error = {e}")
-                continue
+            # ------- uncomment if want to remove original video -----------
+            # try:
+            #     os.remove(audio_filepath)
+            #     print(f"Removed original video")
+            # except Exception as e:
+            #     print(f"Failed to remove original video, error = {e}")
+            #     continue
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    AUDIO_DIR = os.getenv("AUDIO_DIR")
-    SPLIT_DIR = os.getenv("SPLIT_DIR")
-
-    split_audio(
-        vad_dir="./output",
-        audio_dir=AUDIO_DIR,
-        outdir_dir=SPLIT_DIR,
-    )
+    split_audio()
